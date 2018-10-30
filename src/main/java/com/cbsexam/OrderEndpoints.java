38244ed1a405b952c3b5f74e13a5aa9a308ee55e
tobias <tobias.reinert@icloud.com>
2018-10-30 15:07:12 +0100

    Første commit

--- src/main/java/com/cbsexam/OrderEndpoints.java
+++ src/main/java/com/cbsexam/OrderEndpoints.java
@@ -11,6 +11,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import model.Order;
+import utils.Encryption;
 
 @Path("order")
 public class OrderEndpoints {
@@ -29,6 +30,8 @@ public class OrderEndpoints {
     // TODO: Add Encryption to JSON
     // We convert the java object to json with GSON library imported in Maven
     String json = new Gson().toJson(order);
+
+    json = Encryption.encryptDecryptXOR(json);
 
     // Return a response with status 200 and JSON as type
     return Response.status(200).type(MediaType.APPLICATION_JSON).entity(json).build();
